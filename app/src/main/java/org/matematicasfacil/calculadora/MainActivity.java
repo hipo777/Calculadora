3fb303a6257dea083cab9973605a288021dc4614
hipolito <huaylla7@hotmail.com>
2023-06-18 17:06:05 -0400

    Individual 4

--- app/src/main/java/org/matematicasfacil/calculadora/MainActivity.java
+++ app/src/main/java/org/matematicasfacil/calculadora/MainActivity.java
@@ -2,83 +2,74 @@ package org.matematicasfacil.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText edit_dato_uno;
-    private EditText edit_dato_dos;
-    private TextView text_developer;
-    private TextView text_resultado;
-
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ImageView img_imagen = findViewById(R.id.imageView);
-        edit_dato_uno = findViewById(R.id.editTextText1);
-        edit_dato_dos = findViewById(R.id.editTextText2);
+        EditText dato1 = findViewById(R.id.editTextText1);
+        EditText dato2 = findViewById(R.id.editTextText2);
         Button btn_adicion = findViewById(R.id.button1);
         Button btn_sustraccion = findViewById(R.id.button2);
         Button btn_multiplicacion = findViewById(R.id.button3);
         Button btn_division = findViewById(R.id.button4);
-        text_developer = findViewById(R.id.textDeveloper);
-        text_resultado = findViewById(R.id.textView);
+        Button btn_cerrar = findViewById(R.id.button5);
+        TextView resultado = findViewById(R.id.textView);
         img_imagen.setImageResource(R.drawable.calculator);
 
-        btn_adicion.setOnClickListener(v -> {
-            int sum = suma();
-            text_developer.setVisibility(View.INVISIBLE);
-            text_resultado.setVisibility(View.VISIBLE);
-            text_resultado.setText(String.valueOf(sum));
-        });
-        btn_sustraccion.setOnClickListener(v -> {
-            int res = resta();
-            text_developer.setVisibility(View.INVISIBLE);
-            text_resultado.setVisibility(View.VISIBLE);
-            text_resultado.setText(String.valueOf(res));
-        });
-        btn_multiplicacion.setOnClickListener(v -> {
-            int pro = producto();
-            text_developer.setVisibility(View.INVISIBLE);
-            text_resultado.setVisibility(View.VISIBLE);
-            text_resultado.setText(String.valueOf(pro));
-        });
-        btn_division.setOnClickListener(v -> {
-            int coc = cociente();
-            text_developer.setVisibility(View.INVISIBLE);
-            text_resultado.setVisibility(View.VISIBLE);
-            text_resultado.setText(String.valueOf(coc));
-        });
-    }
+        Operacion suma = (a, b) -> a + b;
+        Operacion resta = (a, b) -> a - b;
+        Operacion multiplicacion = (a, b) -> a * b;
+        Operacion division = (a, b) -> a / b;
 
-    public int suma() {
-        int number1 = Integer.parseInt(edit_dato_uno.getText().toString());
-        int number2 = Integer.parseInt(edit_dato_dos.getText().toString());
-        return number1 + number2;
-    }
+        View.OnClickListener click = v -> {
 
-    public int resta() {
-        int number1 = Integer.parseInt(edit_dato_uno.getText().toString());
-        int number2 = Integer.parseInt(edit_dato_dos.getText().toString());
-        return number1 - number2;
-    }
+            if (v==btn_cerrar){
+                finish();// Cierra la actividad actual
+                //finishAffinity(); // Finaliza todas las actividades en el stack
+                //System.exit(0); // Cierra por completo la aplicación
+            }
 
-    public int producto() {
-        int number1 = Integer.parseInt(edit_dato_uno.getText().toString());
-        int number2 = Integer.parseInt(edit_dato_dos.getText().toString());
-        return number1 * number2;
-    }
+            if (v==btn_adicion & v==btn_sustraccion & v==btn_multiplicacion & v==btn_division &
+                dato1.getText().toString().isBlank()||dato2.getText().toString().isBlank()){
+                Toast.makeText(getBaseContext(),"Falta ingresar números", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
-    public int cociente() {
-        int number1 = Integer.parseInt(edit_dato_uno.getText().toString());
-        int number2 = Integer.parseInt(edit_dato_dos.getText().toString());
-        return number1 / number2;
-    }
+            int n1 = Integer.parseInt(dato1.getText().toString());
+            int n2 = Integer.parseInt(dato2.getText().toString());
 
+            if(v==btn_division & n2==0){
+                Toast.makeText(getBaseContext(),"No se puede dividir por cero", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if(v == btn_adicion){
+                resultado.setText(suma.apply(n1, n2) + "");
+            } else if (v == btn_sustraccion) {
+                resultado.setText(resta.apply(n1, n2) + "");
+            } else if (v == btn_multiplicacion) {
+                resultado.setText(multiplicacion.apply(n1, n2) + "");
+            } else if (v == btn_division) {
+                resultado.setText(division.apply(n1, n2) + "");
+            }
+        };
+        btn_adicion.setOnClickListener(click);
+        btn_sustraccion.setOnClickListener(click);
+        btn_multiplicacion.setOnClickListener(click);
+        btn_division.setOnClickListener(click);
+        btn_cerrar.setOnClickListener(click);
+    }
 }
---
+++ app/src/main/java/org/matematicasfacil/calculadora/Operacion.java
@@ -0,0 +1,5 @@
+package org.matematicasfacil.calculadora;
+
+public interface Operacion {
+    int apply(int number1, int number2);
+}
