c9d1bfcd1dc16927afe393245061acfb5d35961c
hipolito <huaylla7@hotmail.com>
2023-07-12 01:01:37 -0400

    Convertido a Kotlin

--- app/src/main/java/org/matematicasfacil/calculadora/MainActivity.kt
+++ app/src/main/java/org/matematicasfacil/calculadora/MainActivity.kt
@@ -1,61 +1,57 @@
-package org.matematicasfacil.calculadora;
+package org.matematicasfacil.calculadora
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import org.matematicasfacil.calculadora.databinding.ActivityMainBinding;
-public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.matematicasfacil.calculadora.databinding.ActivityMainBinding
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        View.OnClickListener click = v -> {
-
-            if (v==binding.btnCerrar){
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(
+            layoutInflater
+        )
+        setContentView(binding!!.root)
+        val click = View.OnClickListener { v: View ->
+            if (v === binding!!.btnCerrar) {
                 //finish();// Cierra la actividad actual
                 //finishAffinity(); // Finaliza todas las actividades en el stack
                 //System.exit(0); // Cierra por completo la aplicación
-                finishAndRemoveTask();//finaliza actividad actual y eliminaa de la pila de actividades
+                finishAndRemoveTask() //finaliza actividad actual y eliminaa de la pila de actividades
             }
-
-            if (v==binding.btnAdicion & v==binding.btnSustraccion & v==binding.btnMultiplicacion & v==binding.btnAdicion &
-                    binding.eT1.getText().toString().isBlank()||binding.eT2.getText().toString().isBlank()){
-                Toast.makeText(getBaseContext(),"Falta ingresar números", Toast.LENGTH_SHORT).show();
-                return;
+            if ((v === binding!!.btnAdicion) and (v === binding!!.btnSustraccion) and (v === binding!!.btnMultiplicacion) and (v === binding!!.btnAdicion) and
+                binding!!.eT1!!.text.toString().isBlank() || binding!!.eT2!!.text.toString()
+                    .isBlank()
+            ) {
+                Toast.makeText(baseContext, "Falta ingresar números", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
             }
-
-            double n1 = Integer.parseInt(binding.eT1.getText().toString());
-            double n2 = Integer.parseInt(binding.eT2.getText().toString());
-
-            if(v==binding.btnDivision & n2==0){
-                Toast.makeText(getBaseContext(),"ERROR", Toast.LENGTH_SHORT).show();
-                return;
+            val n1 = binding!!.eT1!!.text.toString().toInt().toDouble()
+            val n2 = binding!!.eT2!!.text.toString().toInt().toDouble()
+            if ((v === binding!!.btnDivision) and (n2 == 0.0)) {
+                Toast.makeText(baseContext, "ERROR", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
             }
-
-            if(v == binding.btnAdicion){
-                double result = n1 + n2;
-                binding.tvResultado.setText(String.valueOf(result));
-            } else if (v == binding.btnSustraccion) {
-                double result = n1 - n2;
-                binding.tvResultado.setText(String.valueOf(result));
-            } else if (v == binding.btnMultiplicacion) {
-                double result = n1 * n2;
-                binding.tvResultado.setText(String.valueOf(result));
-            } else if (v == binding.btnDivision) {
-                double result = n1 / n2;
-                binding.tvResultado.setText(String.valueOf(result));
+            if (v === binding!!.btnAdicion) {
+                val result = n1 + n2
+                binding!!.tvResultado!!.text = result.toString()
+            } else if (v === binding!!.btnSustraccion) {
+                val result = n1 - n2
+                binding!!.tvResultado!!.text = result.toString()
+            } else if (v === binding!!.btnMultiplicacion) {
+                val result = n1 * n2
+                binding!!.tvResultado!!.text = result.toString()
+            } else if (v === binding!!.btnDivision) {
+                val result = n1 / n2
+                binding!!.tvResultado!!.text = result.toString()
             }
-        };
-
-        binding.btnAdicion.setOnClickListener(click);
-        binding.btnSustraccion.setOnClickListener(click);
-        binding.btnMultiplicacion.setOnClickListener(click);
-        binding.btnDivision.setOnClickListener(click);
-        binding.btnCerrar.setOnClickListener(click);
+        }
+        binding!!.btnAdicion!!.setOnClickListener(click)
+        binding!!.btnSustraccion!!.setOnClickListener(click)
+        binding!!.btnMultiplicacion!!.setOnClickListener(click)
+        binding!!.btnDivision!!.setOnClickListener(click)
+        binding!!.btnCerrar!!.setOnClickListener(click)
     }
 }
